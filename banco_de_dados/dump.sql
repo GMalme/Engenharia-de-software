6b47ce136f62afc27aca41e7f778d7404164d5f7
matheusunb <alextexugo@gmail.com>
2021-10-10 17:22:08 -0300

    atualização

--- banco_de_dados/dump.sql
+++ banco_de_dados/dump.sql
@@ -1,11 +1,24 @@
-CREATE DATABASE teatro;
-USE tatro;
+CREATE DATABASE IF NOT EXISTS teatro;
+USE teatro;
 
 CREATE TABLE IF NOT EXISTS `teatro`.`participantes` (
-  `matricula` INT NOT NULL AUTO_INCREMENT,
+  `idParticipante` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
   `sobrenome` VARCHAR(100) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
   `telefone` VARCHAR(20) NOT NULL,
-  `data_cadastro` DATETIME NOT NULL,
-  PRIMARY KEY (`matricula`));
+  `senha` VARCHAR(20) NOT NULL,
+  `cargo` VARCHAR(20) NOT NULL,
+  `idPeca` INT NOT NULL,
+  PRIMARY KEY (`idParticipante`)
+  );
+
+  CREATE TABLE IF NOT EXISTS `peca`.`participantes` (
+  `idPeca` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `classificacao` VARCHAR(100) NOT NULL,
+  `tipo` VARCHAR(100) NOT NULL,
+  `idParticipante` INT NOT NULL,
+  `idSessao` INT NOT NULL,
+  PRIMARY KEY (`idPeca`)
+  );
